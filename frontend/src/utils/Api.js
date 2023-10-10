ffc5a5447bd2e1173324278befb5320a414b93e9
lenuser <elena-lisina1991@yandex.ru>
2023-10-10 20:30:10 +0700

    finish project15 deploy

---
+++ frontend/src/utils/Api.js
@@ -0,0 +1,114 @@
+class Api {
+    constructor(options) {
+      this._baseUrl = options.baseUrl
+    }
+  
+    _checkResponse(res) {
+      if (res.ok) {
+        return Promise.resolve(res.json())
+      }
+  
+      return Promise.reject(`Ошибка: ${res.status}`)
+    }
+  
+    async getInfo() {
+      const response = await fetch(`${this._baseUrl}/users/me`, {
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+      })
+      return this._checkResponse(response)
+    }
+  
+    async getCards() {
+      const response = await fetch(`${this._baseUrl}/cards`, {
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+      })
+      return this._checkResponse(response)
+    }
+  
+    async setUserInfo(data) {
+      const response = await fetch(`${this._baseUrl}/users/me`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+        body: JSON.stringify({
+          name: data.name,
+          about: data.about,
+        }),
+      })
+      return this._checkResponse(response)
+    }
+  
+    async addCard(data) {
+      const response = await fetch(`${this._baseUrl}/cards`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+        body: JSON.stringify(data),
+      })
+      return this._checkResponse(response)
+    }
+  
+    async addLike(cardId) {
+      const response = await fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+      })
+      return this._checkResponse(response)
+    }
+  
+    async removeCard(cardId) {
+      const response = await fetch(`${this._baseUrl}/cards/${cardId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+      })
+      return this._checkResponse(response)
+    }
+  
+    async deleteLike(cardId) {
+      const response = await fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+      })
+      return this._checkResponse(response)
+    }
+  
+    async setAvatarNew(data) {
+      const response = await fetch(`${this._baseUrl}/users/me/avatar`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+        body: JSON.stringify({
+          avatar: data.avatar,
+        }),
+      })
+      return this._checkResponse(response)
+    }
+  }
+  
+  const api = new Api({
+   baseUrl: "https://lis.back.nomoredomainsrocks.ru",
+    //baseUrl: "http://localhost:3000",
+  })
+  
+  export default api
