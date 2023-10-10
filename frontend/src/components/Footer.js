865c54042bf50f8901f8d2ab76d34f47ce978e72
lenuser <elena-lisina1991@yandex.ru>
2023-10-10 22:30:33 +0700

    hhhhh

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer page__footer">
       <p className="footer__copyright">
-        © {new Date().getFullYear()}. Елена Осипова
+        © {new Date().getFullYear()}. Елена Лисина
       </p>
     </footer>
   )
